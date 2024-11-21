bfeac50e27da31ce47478727edfe0d048fbe5071
kouphasi <phasi.ok.phasi@gmail.com>
2024-11-22 00:32:38 +0900

    setup: directory

--- server/constants/card_creators.js
+++ server/constants/card_creators.js
@@ -2,7 +2,7 @@
 
 import NumCard from '../model/num_card.js';
 import specialCardCreators from './special_card_creators.js';
-import colors from '../constants/colors.js';
+import colors from './colors.js';
 
 const availableNumbers = Array.from({length: 10}, (_, i) => i);
 
